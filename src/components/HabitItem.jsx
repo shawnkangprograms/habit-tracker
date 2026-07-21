95455bc1ebeba686a7de5bd5a69a72a7953f4fa7
Shawn Njoroge <njorogeshawn4@gmail.com>
2026-07-21 09:02:36 +0300

    Stage 8: calendar heatmap visualization with rolling 6-month window

--- src/components/HabitItem.jsx
+++ src/components/HabitItem.jsx
@@ -1,14 +1,36 @@
 import {useHabitCheckoffs} from "../hooks/useHabitCheckoffs";
 import {calculateStreak} from "../utils/streak";
+import "react-calendar-heatmap/dist/styles.css";
+import CalendarHeatmap from "react-calendar-heatmap";
 
 export function HabitItem({habit, uid, isCheckedOffToday, onToggle}) {
     const {habitCheckoffs} = useHabitCheckoffs(uid, habit.id);
     const streak = calculateStreak(habitCheckoffs);
 
+    //WRONG - date doesn't exist as a standalone variable and
+    //[{date: "...", count 1}] is a hardcoded array, not a transformation 
+
+    {/*date.toISOString().split("T")[0];
+    [{date: "...", count: 1} ]*/}
+
+    //CORRECT - But I'm getting a "'heatmapData' is assigned a value but never used" error message.
+    const heatmapData = habitCheckoffs.map((c) => ({
+        date: c.date.toDate().toISOString().split("T")[0],
+        count: 1,
+    }));
+
+    const sixMonthsAgo = new Date();
+    sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+
     return (
         <li>
             <p>Name: {habit.name}</p>
             <p>Current streak: {streak} days </p>
+            <CalendarHeatmap
+                startDate = {sixMonthsAgo}
+                endDate = {new Date()}
+                values={heatmapData}
+            />         
             <button onClick={() => onToggle(habit.id)}>
                 {isCheckedOffToday ? "Done today" : "Mark as done"}
             </button>
